022a30e435c60daed8b66f5e895a68410da0d1b4
TamNhien <canhthien91@gmail.com>
2024-03-29 19:27:07 +0700

    Add project files.

---
+++ p_ViTriDauTienGiaTriK.py
@@ -0,0 +1,6 @@
+﻿def find_first_occurrence(lst, k):
+    return lst.index(k) if k in lst else -1
+
+nums = list(map(int, input("Nhập các số nguyên, cách nhau bởi dấu cách: ").split()))
+k = int(input("Nhập số nguyên dương k: "))
+print(f"Vị trí của phần tử đầu tiên có giá trị {k} là: {find_first_occurrence(nums, k)}")
